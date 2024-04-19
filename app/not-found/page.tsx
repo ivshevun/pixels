30715a2e87c3ce4d41eddf5eb5d5f3ffadea2573
ivsheva <sevcenkoivan673@gmail.com>
2024-04-19 16:46:48 +0300

    Remake 404 page

--- app/not-found/page.tsx
+++ app/not-found/page.tsx
@@ -1,45 +1,42 @@
-"use client";
-import errorImage from "@/public/assets/404.jpg";
-import { Button, Flex, Heading, Link, Text } from "@radix-ui/themes";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import logo from "@/public/logo.jpg";
+import { Button, Flex, Heading, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import { GoArrowLeft } from "react-icons/go";
 
 export default function NotFoundPage() {
-  const router = useRouter();
-
   return (
-    <Flex
-      direction="column"
-      align="center"
-      className="w-full h-screen border relative py-48 gap-4"
-    >
-      <Link href="/">
-        <Image
-          src={logo}
-          alt="Pixels"
-          className="absolute top-7 left-7 w-36 opacity-70 select-none"
-        />
-      </Link>
-      <Image src={errorImage} alt="404" />
-      <Heading className="text-4xl md:text-6xl">Oooops!</Heading>
-      <Text className="text-2xl md:text-3xl font-bold text-center">
-        Looks like you are in the wrong place
-      </Text>
-      <Button
-        color="purple"
-        size={{ initial: "3", md: "4" }}
-        onClick={() => router.push("/")}
-        className="cursor-pointer"
+    <Flex className="w-full h-screen justify-between overflow-hidden">
+      <Flex
+        direction="column"
+        className="justify-center items-center border flex-1 h-screen gap-4 px-6 lg:px-0"
       >
-        Take me home
-      </Button>
-      <Text className="absolute bottom-0 right-2 text-xs opacity-70 flex gap-2 items-center">
-        Yeah, I totally stole it from
-        <Link className="text-purple-500" href="https://batch.com/">
-          Batch
+        <Text className="text-zinc-300 text-sm">404 error</Text>
+        <Heading className="text-3xl sm:text-4xl md:text-5xl 2xl:text-7xl w-full lg:w-3/4 xl:w-1/2 text-center">
+          Something`s missing
+        </Heading>
+        <Text className="w-full lg:w-1/2 xl:w-1/3 text-center">
+          You are seeing this page because the URL you entered doesn`t exist.
+        </Text>
+        <Link href="/">
+          <Button
+            size="4"
+            radius="full"
+            color="purple"
+            className="cursor-pointer"
+          >
+            <GoArrowLeft />
+            Back to home
+          </Button>
         </Link>
-      </Text>
+      </Flex>
+      <video
+        className="object-cover hidden xl:block"
+        autoPlay
+        muted
+        playsInline
+        loop
+      >
+        <source src="/assets/404.mp4" type="video/mp4" />
+      </video>
     </Flex>
   );
 }
