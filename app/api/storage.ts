3c6d9d1107c69de5740cf1239e3ea9c32b0407c7
ivsheva <sevcenkoivan673@gmail.com>
2024-04-27 10:34:08 +0300

    Change aws access key name

--- app/api/storage.ts
+++ app/api/storage.ts
@@ -4,7 +4,7 @@ import { randomUUID } from "crypto";
 export const storageClient = new S3Client({
   region: process.env.AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
