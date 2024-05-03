c896b1fe31c2cf97cba07b080ff63963bb4855be
ivsheva <sevcenkoivan673@gmail.com>
2024-05-03 09:50:28 +0300

    Set up error handling inside /api/shotImage

--- app/api/shotImage/route.ts
+++ app/api/shotImage/route.ts
@@ -1,25 +1,31 @@
+import log from "@/lib/log";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
+  try {
+    const searchParams = request.nextUrl.searchParams;
 
-  const imageUrl = searchParams.get("imageUrl");
+    const imageUrl = searchParams.get("imageUrl");
 
-  if (!imageUrl)
-    return NextResponse.json(
-      { error: "Image URL is required." },
-      { status: 400 }
-    );
+    if (!imageUrl)
+      return NextResponse.json(
+        { error: "Image URL is required." },
+        { status: 400 }
+      );
 
-  const response = await fetch(imageUrl);
+    const response = await fetch(imageUrl);
 
-  if (!response.ok) {
-    return NextResponse.json({ error: "Failed to fetch imageUrl" });
+    if (!response.ok) {
+      return NextResponse.json({ error: "Failed to fetch imageUrl" });
+    }
+
+    const blob = await response.blob();
+    const filename = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+    const file = new File([blob], filename, { type: blob.type });
+
+    return NextResponse.json(file);
+  } catch (error) {
+    log(error);
+    return NextResponse.json(error);
   }
-
-  const blob = await response.blob();
-  const filename = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
-  const file = new File([blob], filename, { type: blob.type });
-
-  return NextResponse.json(file);
 }
